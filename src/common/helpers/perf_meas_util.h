daa04e517141ec36c83e355f66f76afcaf2b1912
Wenhui Zhang <wenhui.zhang@bytedance.com>
2023-06-06 11:53:07 -0700

    add tdx init v1.0.01.01

---
+++ src/common/helpers/perf_meas_util.h
@@ -0,0 +1,15 @@
+// Intel Proprietary 
+// 
+// Copyright 2021 Intel Corporation All Rights Reserved.
+// 
+// Your use of this software is governed by the TDX Source Code LIMITED USE LICENSE.
+// 
+// The Materials are provided “as is,” without any express or implied warranty of any kind including warranties
+// of merchantability, non-infringement, title, or fitness for a particular purpose.
+
+#ifndef PERF_MEAS_UTILS_H
+#define PERF_MEAS_UTILS_H
+
+
+
+#endif // PERF_MEAS_UTILS_H
